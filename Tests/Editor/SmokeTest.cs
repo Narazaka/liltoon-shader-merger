1a65cb8f6f50ceb9533c2d941127dfc0b78cbd4f
Narazaka <info@narazaka.net>
2026-05-12 02:17:01 +0900

    scaffold liltoon-shader-merger package skeleton

---
+++ Tests/Editor/SmokeTest.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace Narazaka.LilToonShaderMerger.Tests
+{
+    public class SmokeTest
+    {
+        [Test]
+        public void Smoke_TestRunnerWorks()
+        {
+            Assert.That(1 + 1, Is.EqualTo(2));
+        }
+    }
+}
