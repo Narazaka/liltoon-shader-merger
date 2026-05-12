d09a4d95a43329bde1df18bcca206a33863318a5
Narazaka <info@narazaka.net>
2026-05-12 16:56:32 +0900

    add LilToonShaderMergerSettings ScriptableObject schema

---
+++ Editor/LilToonShaderMergerSettings.cs
@@ -0,0 +1,31 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace Narazaka.LilToonShaderMerger
+{
+    public enum ConflictStrategy { ErrorOut, PreferFirst, PreferLast }
+    public enum InspectorStrategy { MergeOrSkip }
+
+    [CreateAssetMenu(menuName = "lilToon Shader Merger/Merger Settings", fileName = "ShaderMergerSettings")]
+    public class LilToonShaderMergerSettings : ScriptableObject
+    {
+        [Header("Input")]
+        public DefaultAsset[] sourceFolders;
+
+        [Header("Output")]
+        public string shaderName = "";
+        public DefaultAsset outputFolder;
+        public string editorClassName = "";
+
+        [Header("Conflict Strategy")]
+        public ConflictStrategy propertyConflict = ConflictStrategy.ErrorOut;
+        public ConflictStrategy functionConflict = ConflictStrategy.ErrorOut;
+        public ConflictStrategy replaceConflict  = ConflictStrategy.ErrorOut;
+        public ConflictStrategy textureConflict  = ConflictStrategy.ErrorOut;
+
+        [Header("Options")]
+        public bool dedupeIdenticalIncludes = true;
+        public bool copyExtraFiles = false;
+        public InspectorStrategy inspectorStrategy = InspectorStrategy.MergeOrSkip;
+    }
+}
---
+++ Tests/Editor/SettingsTests.cs
@@ -0,0 +1,23 @@
+using NUnit.Framework;
+using UnityEditor;
+using UnityEngine;
+
+namespace Narazaka.LilToonShaderMerger.Tests
+{
+    public class SettingsTests
+    {
+        [Test]
+        public void CreateInstance_HasDefaults()
+        {
+            var s = ScriptableObject.CreateInstance<LilToonShaderMergerSettings>();
+            Assert.That(s.shaderName, Is.Empty);
+            Assert.That(s.dedupeIdenticalIncludes, Is.True);
+            Assert.That(s.copyExtraFiles, Is.False);
+            Assert.That(s.propertyConflict, Is.EqualTo(ConflictStrategy.ErrorOut));
+            Assert.That(s.functionConflict, Is.EqualTo(ConflictStrategy.ErrorOut));
+            Assert.That(s.replaceConflict,  Is.EqualTo(ConflictStrategy.ErrorOut));
+            Assert.That(s.textureConflict,  Is.EqualTo(ConflictStrategy.ErrorOut));
+            Object.DestroyImmediate(s);
+        }
+    }
+}
